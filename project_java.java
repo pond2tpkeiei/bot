cb5de8c9fcee4cc9e9610a74c9822b0af5f0d780
Pattarapon <151112816+pond2tpkeiei@users.noreply.github.com>
2024-03-13 16:57:57 +0700

    Add files via upload

---
+++ project_java.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class project_java {
+    private static Scanner scanner = new Scanner(System.in);
+    private static String[] services = {
+            "นวดหัว",
+            "นวดมือ",
+            "นวดเท้า",
+            "นวดเท้าและมือ",
+            "นวดหลัง",
+            "นวดไหล่",
+            "นวดหลังและนวดไหล่",
+            "นวดแผนไทย",
+            "นวดแผนไทยผสมผสาน",
+            "นวดประคบสมุนไพร",
+            "นวดน้ำมันอโรม่า",
+            "นวดน้ำมันมะพร้าว",
+            "นวดน้ำมันอโรม่าทองคำ",
+            "นวดออฟฟิศซินโดรม",
+            "นวดสปอร์ต",
+            "นวดสวีดิช",
+            "นวดคุณแม่ตั้งครรภ์",
+            "นวดหินร้อน",
+            "นวดสี่มือ",
+            "เซ็ตสปา(สครับ+อโรม่า)",
+            "สปาคู่รัก",
+            "เลดี้สปา",
+            "สปาสภาพบุรษ",
+            "สปาหน้า"
+    };
+    private static String[] servicesEng ={"Head massage",
+            "Hand massage",
+            "foot massage",
+            "Foot and hand massage",
+            "back massage",
+            "Shoulder massage",
+            "Back massage and shoulder massage",
+            "Thai massage",
+            "Thai traditional massage combined",
+            "Herbal compress massage",
+            "Aroma oil massage",
+            "Coconut oil massage",
+            "Golden Aroma Oil Massage",
+            "Office massage syndrome",
+            "Sports massage",
+            "Swedish massage",
+            "Massage for pregnant mothers",
+            "Hot stone massage",
+            "Four hands massage",
+            "Spa set (scrub + aroma)",
+            "Lovers Spa",
+            "Lady Spa",
+            "Men's Spa",
+            "Facial spa"};
+    public static void main(String[] args) {
+        System.out.println("ยินดีต้อนรับสู่ระบบจองร้านสปา!");
+    }
+}
